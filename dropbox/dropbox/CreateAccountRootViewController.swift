c5eafdd9700dd007e1c7232b63661edeff395cf4
Charlie Liang Yuan <charlie.liang.yuan@gmail.com>
2016-02-07 13:28:04 -0800

    starting create account/signin

---
+++ dropbox/dropbox/CreateAccountRootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreateAccountRootViewController.swift
+//  dropbox
+//
+//  Created by Charlie Yuan on 2/7/16.
+//  Copyright © 2016 Charlie Yuan. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ dropbox/dropbox/SignInRootViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignInRootViewController.swift
+//  dropbox
+//
+//  Created by Charlie Yuan on 2/7/16.
+//  Copyright © 2016 Charlie Yuan. All rights reserved.
+//
+
+import UIKit
+
+class SignInRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
