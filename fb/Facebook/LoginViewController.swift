671ad845444a2762778b7a2fd633331992bc3d9d
Charlie Liang Yuan <charlie.liang.yuan@gmail.com>
2016-02-10 19:15:06 -0800

    added login in segue

--- fb/Facebook/LoginViewController.swift
+++ fb/Facebook/LoginViewController.swift
@@ -13,6 +13,11 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var fieldParentView: UIView!
     @IBOutlet weak var labelParentView: UIView!
     @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
     
     var fieldParentViewInitialY: CGFloat!
     var labelParentViewInitialY: CGFloat!
@@ -30,6 +35,7 @@ class LoginViewController: UIViewController {
         fieldParentViewInitialY = fieldParentView.frame.origin.y
         labelParentViewInitialY = labelParentView.frame.origin.y
         logoInitialY = logo.frame.origin.y
+        self.spinner.stopAnimating()
     }
     
     func keyboardWillShow(notification: NSNotification!) {
@@ -44,7 +50,36 @@ class LoginViewController: UIViewController {
         logo.frame.origin.y = logoInitialY
     }
     
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(),
+            closure
+        )
+        
+    }
+    
+    func checkPassword() {
+        self.spinner.stopAnimating()
+        if (email.text == "admin" && password.text == "password") {
+            self.performSegueWithIdentifier("loggedInSegue", sender: nil)
+        } else {
+            self.loginBtn.selected = false
+        }
+    }
+    
     @IBAction func didTap(sender: UITapGestureRecognizer) {
         view.endEditing(true)
+    }
+    
+    @IBAction func onLoginClick(sender: AnyObject) {
+        self.spinner.startAnimating()
+        delay(2) {
+            self.checkPassword()
+        }
+        
     }
 }
