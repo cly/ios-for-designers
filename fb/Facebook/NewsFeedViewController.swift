e63de97a764a32235f414dc65b3a57458e26df04
Charlie Liang Yuan <charlie.liang.yuan@gmail.com>
2016-02-10 20:02:35 -0800

    step 8

--- fb/Facebook/NewsFeedViewController.swift
+++ fb/Facebook/NewsFeedViewController.swift
@@ -13,11 +13,41 @@ class NewsFeedViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var feedImageView: UIImageView!
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    
+    func delay(delay:Double, closure:()->()) {
+        dispatch_after(
+            dispatch_time(
+                DISPATCH_TIME_NOW,
+                Int64(delay * Double(NSEC_PER_SEC))
+            ),
+            dispatch_get_main_queue(),
+            closure
+        )
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Configure the content size of the scroll view
         scrollView.contentSize = CGSizeMake(320, feedImageView.image!.size.height)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.activityIndicator.startAnimating()
+        self.scrollView.hidden = true
+        self.activityIndicator.hidden = false
+        
+        delay(2){
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.hidden = true
+            self.scrollView.hidden = false
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
